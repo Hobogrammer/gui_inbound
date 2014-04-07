28fac144fdf7b6de45dc84f53931b31194003692
Andre Smith <andre@generalui.com>
2014-04-07 10:18:04 -0700

    Change action to post, include postmark

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.post('/inbound', function(req, res) {
+  var incomingMail = '';
+  req.on('data', function(data){
+    incomingMail += data;
+  });
+
+  req.on('end', function(){
+    var mailJSON = JSON.parse(incomingMail);
+
+    console.log(mailJSON);
+  });
 });
 
 var port = Number(process.env.PORT || 5000);
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
